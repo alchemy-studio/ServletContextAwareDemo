da245842d65ad4bcfdd3fc9bb7ae1b7683ac580a
阿男 <l.weinan@gmail.com>
2019-11-26 20:00:05 +0800

    add doc

--- src/main/java/io/weli/spring/MyContextAware.java
+++ src/main/java/io/weli/spring/MyContextAware.java
@@ -5,6 +5,7 @@ import org.springframework.web.context.ServletContextAware;
 
 import javax.servlet.ServletContext;
 
+// https://books.google.fr/books?id=rjRjDwAAQBAJ&pg=PA343&lpg=PA343&dq=ServletContextAware+usage&source=bl&ots=8mQ2gG_fmQ&sig=ACfU3U3DIef--U-Pwsd8KROQQ2_PSwy6uA&hl=en&sa=X&ved=2ahUKEwjl0f745YfmAhVRXn0KHRuZAHEQ6AEwCHoECAoQAQ#v=onepage&q=ServletContextAware%20usage&f=false
 @Component
 public class MyContextAware implements ServletContextAware {
 
